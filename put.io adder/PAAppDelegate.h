8178447279ef202b0295d2bc72c30e4b469aa27f
Max Winde <max.winde@gmail.com>
2013-07-23 11:39:24 +0200

    Initial Commit

---
+++ put.io adder/PAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PAAppDelegate.h
+//  put.io adder
+//
+//  Created by Max Winde on 23.07.13.
+//  Copyright (c) 2013 343max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ put.io adder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  put.io adder
+//
+//  Created by Max Winde on 23.07.13.
+//  Copyright (c) 2013 343max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PAAppDelegate class]));
+    }
+}
