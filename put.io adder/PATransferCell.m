023c59d1d49910fbcfd20384bc182c4e8e892756
Max Winde <max.winde@gmail.com>
2013-07-24 18:54:02 +0200

    added the remaining time to a download

--- put.io adder/PATransferCell.m
+++ put.io adder/PATransferCell.m
@@ -12,6 +12,25 @@
 
 @implementation PATransferCell
 
+- (NSString *)formatedTime:(NSInteger)seconds;
+{
+    if (seconds < 20) {
+        return NSLocalizedString(@"a few seconds", nil);
+    } else if (seconds < 60) {
+        return NSLocalizedString(@"less then a minute", nil);
+    } else if (seconds < 120) {
+        return NSLocalizedString(@"one minute", nil);
+    }  else if (seconds < 3600) {
+        NSInteger minutes = floorf(seconds / 60.0);
+        return [NSString stringWithFormat:NSLocalizedString(@"%i minutes", nil), minutes];
+    } else {
+        NSInteger hours = floorf(seconds / 3600.0);
+        seconds -= hours * 3600;
+        NSInteger minutes = floorf(seconds / 60.0);
+        return [NSString stringWithFormat:NSLocalizedString(@"%i h %i m", nil), hours, minutes];
+    }
+}
+
 - (void)setTransfer:(PKTransfer *)transfer;
 {
     if (transfer == _transfer) return;
@@ -21,7 +40,7 @@
     self.textLabel.textColor = (transfer.transferStatus == PKTransferStatusCompleted ? [UIColor grayColor] : [UIColor blackColor]);
     self.textLabel.text = transfer.name;
     
-    if (transfer.transferStatus != PKTransferStatusCompleted && transfer.transferStatus != PKTransferStatusQueued) {
+    if (transfer.transferStatus != PKTransferStatusCompleted && transfer.transferStatus != PKTransferStatusQueued && transfer.transferStatus != PKTransferStatusDownloading) {
         self.detailTextLabel.text = transfer.statusMessage;
     }
     
@@ -29,6 +48,16 @@
         LSRoundProgressView *progressView = [[LSRoundProgressView alloc] initWithFrame:CGRectMake(0.0, 0.0, 38.0, 38.0)];
         progressView.progress = [transfer.percentDone floatValue] / 100.0;
         self.accessoryView = progressView;
+        
+        NSInteger secondsRemaining = [transfer.estimatedTime integerValue];
+        NSString *remainingString;
+        if (secondsRemaining <= 0) {
+            remainingString = @"∞";
+        } else {
+            remainingString = [self formatedTime:secondsRemaining];
+        }
+        
+        self.detailTextLabel.text = [NSString stringWithFormat:NSLocalizedString(@"%@ remaining", nil), remainingString];
     }
 
     if (transfer.transferStatus == PKTransferStatusCompleted) {
