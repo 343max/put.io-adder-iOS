dd43b53870419a49236d271fd0e65fb0bc9e18ac
Max Winde <max.winde@gmail.com>
2013-07-29 14:42:16 +0200

    dismissing the search view controller after starting a search

--- put.io adder/PASearchViewController.m
+++ put.io adder/PASearchViewController.m
@@ -59,6 +59,12 @@ NSString * const PASearchViewControllerDefaultSearchTemplate = @"http://archive.
     }
     
     [[UIApplication sharedApplication] openURL:searchURL];
+    
+    double delayInSeconds = 0.3;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self dismissViewControllerAnimated:NO completion:nil];
+    });
 }
 
 
